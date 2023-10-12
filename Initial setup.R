4c47cdb7bb1190345280c94522427d8889ebf7f2
emmakb <emmabradshaw@hotmail.com>
2023-10-12 15:48:23 +0200

    Updated with DFO MPA data

--- Initial setup.R
+++ Initial setup.R
@@ -18,7 +18,8 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(ggspatial)
 
-##Reading in shape files##
+##Reading in KBA and Canadian MPAs#
+
 #Key Biodiversity Areas#
 #Read in KBA data#
 . <- setwd("/Users/emmabradshaw/Desktop/Lund- Thesis/Code/LUThesis/KBAsGlobal_2023_March_01_Criteria_TriggerSpecies")
@@ -31,13 +32,19 @@ st_crs(world)
 worldKBA <-fortify(globalKBAs)
 ggplot() +
   geom_sf(data = world, fill= "black", color= NA) +
-  geom_sf(data=worldKBA, fill="red", group="KBAs") +
+  geom_sf(data=worldKBA, fill="red") +
   coord_sf(xlim= c(-90,-15), ylim= c(15,60), expand= FALSE) +
-  annotation_north_arrow(location = "br", which_north = "true", 
+  annotation_north_arrow(location = "tr", which_north = "true", 
                          pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
                          style = north_arrow_fancy_orienteering) +
-  annotation_scale(location = "tl") +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("Key Biodiversity Areas (KBAs) in Eastern North America")
 
-
+#Read in Canada MPA data#
+. <- setwd("/Users/emmabradshaw/Desktop/Lund- Thesis/Code/LUThesis/DFO_MPA_MPO_ZPM_SHP")
+canadaMPAs <- st_read(dsn = ".", layer = "DFO_MPA_MPO_ZPM")
+#Plot on a map#
+ggplot() +
+  geom_sf(data = world, fill= "black", color= NA) +
+  geom_sf(data=canadaMPAs, fill="red") +
+  coord_sf(xlim= c(-90,-40), ylim= c(40,60), expand= FALSE)
