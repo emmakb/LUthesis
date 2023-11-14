b15591b31e1f3d6137c9ac3a1f0f78b01ed29816
emmakb <emmabradshaw@hotmail.com>
2023-11-14 14:08:45 +0100

    keep getting error message & discovered that many data points lie beyond the timeline of the env data

--- Environmental data.R
+++ Environmental data.R
@@ -14,19 +14,74 @@ library(plotdap)
 library(rerddap)
 library(sp)
 library(rerddapXtracto)
-library(gganimate)
 library(ggplot2)
+library(gganimate)
 library(plotdap)
 
 
+#Open clean species data
+. <- setwd("/Users/emmabradshaw/Desktop/Lund- Thesis/Code/LUThesis")
+load("~/Desktop/Lund- Thesis/Code/LUThesis/mako_df2.Rdata")
+load("~/Desktop/Lund- Thesis/Code/LUThesis/white_subadult2.Rdata")
+load("~/Desktop/Lund- Thesis/Code/LUThesis/white_YOY2.Rdata")
+load("~/Desktop/Lund- Thesis/Code/LUThesis/white_matureF2.Rdata")
+load("~/Desktop/Lund- Thesis/Code/LUThesis/white_matureM2.Rdata")
+
+#Load environmental data
+#chlorophyll a data
+chlaInfo <- rerddap::info('erdMH1chla1day')
+chlaInfo
+#depth data
+depthInfo <- rerddap::info('etopo1_bedrock')
+depthInfo
+#SST data
+SSTInfo <- rerddap::info('erdMH1sstd1dayR20190SQ')
+SSTInfo
+#salinity data
+pHInfo <- rerddap::info('')
+pHInfo
+#_________________________________________________________________________
+
+##WHITE SHARKS#
+#Mature males
+whitemaledata <- white_matureM2
+xposw1 <- white_matureM2$Longitude
+yposw1 <- white_matureM2$Latitude
+tposw1 <- white_matureM2$Timestamp
+
+#Mature females
+whitefemaledata <- white_matureF2
+xposw2 <- whitefemaledata$Longitude
+yposw2 <- whitefemaledata$Latitude
+tposw2 <- c("2013-01-01", "2021-05-01")
+
+#Link to chlorophyll a data
+WFchla <- rxtractogon(chlaInfo, parameter = parameter, xcoord = xposw2, ycoord = yposw2, tcoord = tposw2)
+#Link to SST data
+WFtemp <- rxtractogon(SSTInfo, parameter = "sstMasked", xcoord = xposw2, ycoord = yposw2, tcoord = tposw2)
+
+#Sub-adults
+whitesubadultdata <- white_subadult2
+xposw3 <- whitesubadultdata$Longitude
+yposw3 <- whitesubadultdata$Latitude
+tposw3 <- whitesubadultdata$Timestamp
+
+#YOY
+whiteyoungdata <- white_YOY2
+xposw4 <- whiteyoungdata$Longitude
+yposw4 <- whiteyoungdata$Latitude
+tposw4 <- whiteyoungdata$Timestamp
+
+
+##SHORTFIN MAKOS##
 #First copy in tag data into a variable
 #call it something else so that the subsequent code is more generic
-GWtagdata<- ##
-xpos <- ##
-ypos <- ##
-tpos <-##
-#download sst data
-sstInfo <- info('noaa_psl_04af_50c4_3592')
-sstInfo
-#Combine them
-blabla <- rxtracto(dataInfo, parameter = 'sst', xcoord = xpos, ycoord = ypos, tcoord = tpos, xlen = .2, ylen = .2)
+SFMtagdata<- mako_df2
+xpossf <- SFMtagdata$longitude
+ypossf <- SFMtagdata$latitude
+tpossf <-SFMtagdata$timestamp
+
+#Combine chlorophyll a data to SF makos
+#Combine depth data to SF makos
+#Combine SST data to SF makos
+#Combine salinity data to SF makos
